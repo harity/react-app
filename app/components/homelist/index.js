b2987354a852dab3b764e045f5964d9b49bbc6a0
harity <985463770@qq.com>
2017-12-10 20:30:21 +0800

    20171210

--- app/components/homelist/index.js
+++ app/components/homelist/index.js
@@ -1,20 +1,51 @@
 import React from 'react';
 import './index.css';
-import {Link} from 'react-router'
+import {Link} from 'react-router';
 
 export default class Homelist extends React.Component{
 	constructor() {
 		super();
         this.state = {
-            news: ''
+            news: '',
+            loadmore:true,
+            isloading:false
         }
 		
 	};
-	componentWillMount(){
+	componentDidMount(){
+		var _this = this;
+		let timeoutId;
+		const wrapper = this.refs.wrapper;
         var myFetchOptions = {
 			method: 'GET'
 		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=guoji&count=20", myFetchOptions).then(response => response.json()).then(json => this.setState({news: json}));
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=guoji&count=5", myFetchOptions).then(response => response.json()).then(json => this.setState({news: json}));
+    	function callback() {
+            const top = wrapper.getBoundingClientRect().top
+            const windowHeight = window.screen.height
+            if (top && top < windowHeight) {
+                // 证明 wrapper 已经被滚动到暴露在页面可视范围之内了
+                _this.moremsg()
+            }
+        };
+    	window.addEventListener('scroll', function () {
+            if (this.state.isloading) {
+                return
+            }
+            if (timeoutId) {
+                clearTimeout(timeoutId)
+            }
+            timeoutId = setTimeout(callback, 50)
+        }.bind(this), false);
+    };
+
+    moremsg(){
+    	console.log(111)
+    	var myFetchOptions = {
+			method: 'GET'
+		};
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=guoji&count=5", myFetchOptions).then(response => response.json()).then(json => this.setState({news: this.state.news.concat(json)}));
+    	
     };
 	render() {
 		const {news} = this.state;
@@ -40,6 +71,13 @@ export default class Homelist extends React.Component{
             <div className="homelist">
             	<p>---推荐---</p>
 	            {newsList}
+	            <div className="loadmore" ref="wrapper">
+	            {
+	            	this.state.loadmore
+	            	?<span onClick={this.moremsg.bind(this)} >加载更多</span>
+	            	:<span>加载中...</span>
+	            }
+	            </div>
             </div>
         )
     }
